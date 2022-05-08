96d50dd67015cdac961e2e48ea485eae832f6582
BachDV <bachdv.hust@gmail.com>
2022-05-08 09:51:01 +0700

    add realtime comment

--- app/src/main/java/techlab/ai/hackathon/ui/comment/CommentAdapter.kt
+++ app/src/main/java/techlab/ai/hackathon/ui/comment/CommentAdapter.kt
@@ -3,23 +3,20 @@ package techlab.ai.hackathon.ui.comment
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Comment
+import techlab.ai.hackathon.R
 import techlab.ai.hackathon.common.load
 import techlab.ai.hackathon.data.model.CommentModel
 import techlab.ai.hackathon.databinding.ItemCommentBinding
+import techlab.ai.hackathon.share_ui.avatagen.AvatarGenerator
 
 /**
  * @author BachDV
  */
-class CommentAdapter : RecyclerView.Adapter<CommentViewHolder>() {
-
-    var listData: List<CommentModel> = arrayListOf()
-        @SuppressLint("NotifyDataSetChanged")
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class CommentAdapter : ListAdapter<CommentModel, CommentViewHolder>(CommentDiffCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         return CommentViewHolder(
@@ -32,10 +29,18 @@ class CommentAdapter : RecyclerView.Adapter<CommentViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
-        holder.bindData(listData[position])
+        holder.bindData(getItem(position))
+    }
+}
+
+private class CommentDiffCallBack : DiffUtil.ItemCallback<CommentModel>() {
+    override fun areItemsTheSame(oldItem: CommentModel, newItem: CommentModel): Boolean {
+        return oldItem.id == newItem.id
     }
 
-    override fun getItemCount(): Int = listData.size
+    override fun areContentsTheSame(oldItem: CommentModel, newItem: CommentModel): Boolean {
+        return false
+    }
 }
 
 class CommentViewHolder(private val binding: ItemCommentBinding) :
@@ -45,10 +50,25 @@ class CommentViewHolder(private val binding: ItemCommentBinding) :
             binding.tvContent.text = item.content
             item.user?.let { user ->
                 binding.tvName.text = (user.firstName ?: "") + (user.lastName ?: "")
-                binding.ivAvatar.load(url = user.avatar)
+                user.avatar?.let {
+                    binding.ivAvatar.load(url = it, placeholder = R.drawable.ic_logo_funtap)
+                } ?: kotlin.run {
+                    val av = AvatarGenerator.AvatarBuilder(binding.root.context)
+                        .setLabel(user?.lastName ?: "")
+                        .setAvatarSize(120)
+                        .setTextSize(30)
+                        .toSquare()
+                        .toCircle()
+                        .build()
+                    binding.ivAvatar.setImageDrawable(
+                        av
+                    )
+                }
             } ?: kotlin.run {
                 binding.tvName.text = ""
             }
+
+
         } finally {
 
         }
